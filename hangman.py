0006e26975d6f0f17b8f3c459269e3d3121faaa7
Andras Molnar <andris9016@gmail.com>
2018-12-12 13:44:38 +0100

    Add print_header and print_hangman function

--- hangman.py
+++ hangman.py
@@ -1,4 +1,25 @@
 
+import random
+
+
+def print_header():
+    print("""
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    """)
+
+
+def print_hangman(file="hangmans.txt", nth_pic=0):
+    with open(file, "r") as read_hangman:
+        hangamn_body = read_hangman.read()
+        hangmans_list = hangamn_body.split("@")
+        print(hangmans_list[nth_pic])
+
+
 abc = ("abcdefghijklmnopqrstuvwxyz")
 correct = ("cica")
 wrongs = []
@@ -17,10 +38,11 @@ while wrong_guess < 3:
         else:    
             wrongs.append(guess)
             show_wrong_latters(wrongs)
-        
-    
 
 
+def main():
+    print_header()
+    print_hangman(nth_pic=7)
 
 
-
+main()
